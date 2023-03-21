d1c5be82bcddb9ed2deedea48a636e294f48eff7
Lycoiref <1808347356@qq.com>
2023-03-21 15:04:13 +0800

    测试跨组件调用

--- external/database-postgresql/src/index.ts
+++ external/database-postgresql/src/index.ts
@@ -2,15 +2,17 @@ import { Context, Schema } from 'koishi'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+let user
 
 async function main() {
     // ... you will write your Prisma Client queries here
-    const user = await prisma.bag_users.findMany({
+    user = await prisma.bag_users.findMany({
         where: {
             id: 81,
         }
     })
     console.log(user[0].property)
+    return user
 }
 
 export const name = 'database-postgresql'
@@ -18,6 +20,8 @@ export const name = 'database-postgresql'
 export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
+
+export const users = main()
 
 export async function apply(ctx: Context) {
     // write your plugin here
--- external/lycoiref-test/src/index.ts
+++ external/lycoiref-test/src/index.ts
@@ -1,17 +1,19 @@
 import { Context, Schema } from 'koishi'
+import { users } from 'koishi-plugin-database-postgresql'
 
 export const name = 'lycoiref-test'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
-  // 如果收到“天王盖地虎”，就回应“宝塔镇河妖”
-  ctx.on('message', (session) => {
-    if (session.content === '天王盖地虎') {
-      session.send('test success')
-    }
-  })
+    // write your plugin here
+    ctx.on('message', (session) => {
+        console.log(session);
+        console.log("user is :" + users[0].property)
+        let info = JSON.stringify(users[0].property)
+        console.log(info, "???")
+        session.send(info)
+    })
 }
