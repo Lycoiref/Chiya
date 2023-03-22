9fad3a3d6c0889b8a863b1310b78bf16b5969d45
firstmeet1108 <2022742378@qq.com>
2023-03-22 11:57:53 +0800

    更新插件  “帮助1”！！

--- external/help1/src/index.ts
+++ external/help1/src/index.ts
@@ -1,7 +1,5 @@
 import { Context, Schema } from 'koishi'
 import { h } from 'koishi'
-import path from "path";
-
 //图片转文字的包
 //https://yarnpkg.com/package/ultimate-text-to-image
 import { UltimateTextToImage} from "ultimate-text-to-image"
@@ -14,29 +12,28 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  const textToImage = new UltimateTextToImage("Ultimate Text To Image", {
-    width: 400,
-    maxWidth: 1000,
-    maxHeight: 1000,
-    fontFamily: "Arial",
-    fontColor: "#00FF00",
-    fontSize: 72,
-    minFontSize: 10,
-    lineHeight: 50,
-    autoWrapLineHeightMultiplier: 1.2,
-    margin: 20,
-    marginBottom: 40,
-    align: "center",
-    valign: "middle",
-    borderColor: 0xFF000099,
-    borderSize: 2,
-    backgroundColor: "0080FF33",
-    underlineColor: "#00FFFF33",
-    underlineSize: 2,
-  })
-  textToImage.render().toFile(path.join(__dirname, "image2.png"))
   ctx.command('help1', '帮助1')
-    .action(({ session }) => {
-      session.send(h('image', { url: __dirname +'image2.png' }))
+    .action(async ({ session }) => {
+      let helpString = "1.吃饭" + "\n" + "2.睡觉"+ "\n"  + "3.打豆豆"
+      const textToImage = new UltimateTextToImage(helpString, {
+        width: 400,
+        maxWidth: 1000,
+        maxHeight: 1000,
+        fontFamily: "宋体",
+        fontColor: "#FFFFFF",
+        fontSize: 23,
+        minFontSize: 10,
+        lineHeight: 50,
+        // autoWrapLineHeightMultiplier: 1.2,
+        margin: 20,
+        marginBottom: 40,
+        align: "center",
+        valign: "middle",
+        borderColor: 0xFF000099,
+        borderSize: 2,
+        backgroundColor: "#000000",
+      })
+      let buffer = textToImage.render().toBuffer("image/jpeg")
+      await session.send(h.image(buffer, 'image/png'))
     })
 }
