89cc357355b8d462519fb52fdeef347770df7447
Lycoiref <1808347356@qq.com>
2023-03-23 23:27:38 +0800

    添加了好友申请控制插件和识图插件

---
+++ external/recognizetion-pic/src/index.ts
@@ -0,0 +1,15 @@
+import { Context, Schema } from 'koishi'
+
+export const name = 'recognize-pic'
+
+export interface Config { }
+
+export const Config: Schema<Config> = Schema.object({})
+
+export function apply(ctx: Context) {
+    // write your plugin here
+    // ctx.on("message", async (session) => {
+    //     console.log('***', session.elements[1].attrs)
+    // })
+    ctx.command('识图')
+}
