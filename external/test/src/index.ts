dc9662bb7616431207dacc86ae46191c0be37439
Lycoiref <1808347356@qq.com>
2023-03-24 14:24:55 +0800

    添加anti dlc系统

--- external/test/src/index.ts
+++ external/test/src/index.ts
@@ -9,6 +9,25 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
     // write your plugin here
+    let anti_dlc = false
+    ctx.command('anti_dlc <keep_time>')
+        .action(({ session }, keep_time) => {
+            console.log(keep_time)
+            session.send('已打开反DLC系统')
+            anti_dlc = true
+            // keep_time秒后关闭
+            setTimeout(() => {
+                anti_dlc = false
+                session.send('已关闭反DLC系统')
+            }, Number(keep_time) * 1000)
+        })
+    ctx.middleware(async (session, next) => {
+        if (anti_dlc && session.userId === '1297442101') {
+            session.send('检测到DLC，开始反DLC攻击')
+            return '傻逼dlc'
+        }
+        return next()
+    }, true)
 }
 
 new Logger('error:').info('211')
