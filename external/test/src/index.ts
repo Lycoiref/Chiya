6130010df8996d9d804956db59776e9d8eb483ce
apprehen <3514392356@qq.com>
2023-03-24 15:51:29 +0800

    天气插件1.0.0！！等待新生写样式优化捏

--- external/test/src/index.ts
+++ external/test/src/index.ts
@@ -1,5 +1,5 @@
 import { Context, Schema } from 'koishi'
-import { Logger } from 'koishi'
+import { Logger } from 'koishi' 
 
 export const name = 'test'
 
@@ -8,7 +8,18 @@ export interface Config { }
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-    // write your plugin here
+    ctx.on('message',(session)=>{
+        if (session.content == '傻逼周龙荣' && session.userId != '1253794103') {
+            session.send('傻逼周龙荣')
+        } 
+    })
+    ctx.middleware((session,next)=>{
+        if (session.userId == '1253794103') {
+            session.send('检测到傻逼周龙荣说话')
+        } else {
+            return next()
+        } 
+    })
 }
 
 new Logger('error:').info('211')
