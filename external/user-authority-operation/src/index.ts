2d2fe9bec592fc67a6328e747957050d9a6bf262
Lycoiref <1808347356@qq.com>
2023-03-25 14:05:48 +0800

    修复了无法通过好友申请的bug（bots[1] -> session.bot）

--- external/user-authority-operation/src/index.ts
+++ external/user-authority-operation/src/index.ts
@@ -11,20 +11,5 @@ export interface Config { }
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  ctx.middleware(async (session, next) => {
-    await session.send("ok")
-    if(session.subtype === "group"){
-      if(session.content === ".ban"){
-        session.send("ok")
-      }
-    }
-    // if(session.subtype === "group" && session.guildId === "765557440") {
-    //   let user = await getUserAuthority(parseInt(session.userId),parseInt(session.guildId))
-    //   console.log("user=" + user)
-    //   if(user === null) {
-    //     await setUserAuthority(parseInt(session.userId),parseInt(session.guildId),5)
-    //   }
-    // }
-    
-  })
+
 }
